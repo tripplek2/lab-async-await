3b29edf45848367ddb7f54be8933bdc28c32bafe
Kelvin Korir <kibetkelvin222@gmail.com>
2026-04-13 09:54:04 +0300

    index.js file updated

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your code here!
+const postList = document.getElementById("post-list");
+const loadBtn = document.getElementById("load-btn");
+
+// fetch posts using async
+async function getPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+
+    displayPosts(data);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+// displaying posts
+function displayPosts(posts) {
+  postList.innerHTML = "";
+
+  // Random 10 posts
+  const randomPosts = posts.sort(() => 0.5 - Math.random()).slice(0, 10);
+
+  randomPosts.forEach(post => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <h3>${post.title}</h3>
+      <p>${post.body}</p>
+    `;
+
+    postList.appendChild(li);
+  });
+}
+
+getPosts();
+
+if (loadBtn) {
+  loadBtn.addEventListener("click", getPosts);
+}
